d1c0db513355cd84f5862d49b61a241fff287613
hung34atpvti <hung34atpvti@gmail.com>
2023-01-12 23:12:58 +0700

    event-loop

--- event-loop.js
+++ event-loop.js
@@ -23,11 +23,9 @@ fs.readFile("test-file.txt", () => {
   crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
   console.log(Date.now() - start, "Password encrypted");
 
-  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
-  console.log(Date.now() - start, "Password encrypted");
-
-  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
-  console.log(Date.now() - start, "Password encrypted");
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => console.log(Date.now() - start, "Password encrypted"));
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => console.log(Date.now() - start, "Password encrypted"));
+  
 });
 
 console.log("Hello from the top-level code");
